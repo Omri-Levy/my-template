bde1d5233b01d399814142f88e428474766bcb13
Omri Levy <omri.levy0197@gmail.com>
2021-03-01 12:36:24 +0200

    refactor(currentuserdetail): added a short description to the component
    also now wrapping the component's text with Chakra UI's Text component

--- packages/client/src/components/pages/Profile/CurrentUserDetail/index.tsx
+++ packages/client/src/components/pages/Profile/CurrentUserDetail/index.tsx
@@ -1,7 +1,12 @@
 import { FunctionComponent } from 'react';
-import { ListIcon, ListItem } from '@chakra-ui/react';
+import { ListIcon, ListItem, Text } from '@chakra-ui/react';
 import { Props } from './types';
 
+/**
+ * a component to render a single field of the signed in user's details or
+ * to be mapped - made of Chakra-UI's ListItem, ListIcon and, Text
+ * components. Returns an empty field if no user is authenticated.
+ */
 const CurrentUserDetail: FunctionComponent<Props> = ({
   objectKey,
   icon,
@@ -10,9 +15,13 @@ const CurrentUserDetail: FunctionComponent<Props> = ({
 }) => (
   <ListItem textTransform={objectKey === `role` ? `capitalize` : undefined}>
     <ListIcon as={icon} color={`gray.300`} />
-    {`${text}: ${
-      currentUser && currentUser !== `unauthenticated` && currentUser[objectKey]
-    }`}
+    <Text>
+      {`${text}: ${
+        currentUser &&
+        currentUser !== `unauthenticated` &&
+        currentUser[objectKey]
+      }`}
+    </Text>
   </ListItem>
 );
 
