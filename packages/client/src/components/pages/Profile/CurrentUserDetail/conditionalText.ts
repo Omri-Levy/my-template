89559f98ee075814554d270f62e6524145177058
Omri Levy <omri.levy0197@gmail.com>
2021-03-01 12:59:31 +0200

    docs(conditionaltext.ts): added a short description

--- packages/client/src/components/pages/Profile/CurrentUserDetail/conditionalText.ts
+++ packages/client/src/components/pages/Profile/CurrentUserDetail/conditionalText.ts
@@ -1,5 +1,10 @@
 import { ConditionalText } from './types';
 
+/**
+ * the function decides on a string based on the authentication status of the
+ * user - if the user is authenticated, returns a field from the current map
+ * iteration and returns an empty string if the user is not authenticated.
+ */
 const conditionalText: ConditionalText = (currentUser, text, objectKey) => {
   if (currentUser && currentUser !== `unauthenticated`) {
     return `${text}: ${currentUser[objectKey]}`;
